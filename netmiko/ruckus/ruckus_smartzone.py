21985fc8dc962584e457b854dbda1646fa3dc67b
Ivo Guerreiro <ivo.guerreiro@hoistgroup.com>
2022-04-08 16:30:54 +0100

    change config command for ruckus

--- netmiko/ruckus/ruckus_smartzone.py
+++ netmiko/ruckus/ruckus_smartzone.py
@@ -2,7 +2,16 @@ from netmiko.cisco_base_connection import CiscoBaseConnection, CiscoFileTransfer
 from netmiko.base_connection import BaseConnection
 from netmiko.cisco.cisco_ios import CiscoIosBase
 
+
 class RuckusSmartzoneSSH(CiscoIosBase):
     """Ruckus IOS SSH driver."""
 
-    pass
+    def config_mode(
+        self,
+        config_command: str = "config",
+        pattern: str = "",
+        re_flags: int = 0,
+    ) -> str:
+        return super().config_mode(
+            config_command=config_command, pattern=pattern, re_flags=re_flags
+        )
